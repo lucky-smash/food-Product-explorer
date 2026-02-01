8b9a47ecd2786f167be555cc2cd578128239bbcc
Admin <admin@MacBook-Pro.local>
2026-02-01 09:04:03 +0530

    refactor: move frontend into frontend directory

--- src/services/openFoodApi.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = "https://world.openfoodfacts.org";
-
-export const searchProductsByName = async (query, page = 1) => {
-  const response = await fetch(
-    `${BASE_URL}/cgi/search.pl?search_terms=${query}&page=${page}&json=true`
-  );
-  return response.json();
-};
